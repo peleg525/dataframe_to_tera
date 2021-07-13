6172ec5f8eb00ac324620ff3501f50be418a21df
peleg525 <73443096+peleg525@users.noreply.github.com>
2021-07-13 15:38:47 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
 
 setup(
-name = 'large_dataframe_to_teradata',         # How you named your package folder (MyLib)
-  packages = ['large_dataframe_to_teradata'],   # Chose the same as "name"
+name = 'large_data_to_teradata',         # How you named your package folder (MyLib)
+  packages = ['large_data_to_teradata'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Push large dataframe to teradata',   # Give a short description about your library
   author = 'Peleg Wurzel',                   # Type in your name
   author_email = 'Peleg525@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/peleg525/large_dataframe_to_teradata.git',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/peleg525/large_dataframe_to_teradata/archive/refs/tags/V_01.tar.gz',    # I explain this later on
+  url = 'https://github.com/peleg525/large_data_to_teradata.git',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/peleg525/large_data_to_teradata/archive/refs/tags/V_01.tar.gz',    # I explain this later on
   keywords = ['teradata'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
